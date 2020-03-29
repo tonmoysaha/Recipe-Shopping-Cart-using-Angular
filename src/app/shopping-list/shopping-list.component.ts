d8082abd033addbe01308b157a1a6ff716d322f1
tonmoysaha <tonmoysahaopi@gmail.com>
2020-03-29 12:45:18 +0600

    adding shopping edit and shopping list template

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IngredientModel} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: IngredientModel[] =[
+    new IngredientModel('apples', 4),
+    new IngredientModel('tomatos', 3)
+  ];
   constructor() { }
 
   ngOnInit(): void {
