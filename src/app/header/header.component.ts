46debafa97ecd00901e7ea8194a80c91b6c70a66
tonmoysaha <tonmoysahaopi@gmail.com>
2020-04-10 22:36:37 +0600

    databind for navigate funtions

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import { Subject, Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export  class HeaderComponent {
+  @Output() FeatureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.FeatureSelected.emit(feature);
+
+  }
+
 
 }
