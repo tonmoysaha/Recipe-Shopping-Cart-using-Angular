b6f6f2094660cc8d91df638207373a27200f9800
tonmoysaha <tonmoysahaopi@gmail.com>
2020-11-11 03:37:07 -0800

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RecipesListComponent} from "./recipes/recipes-list/recipes-list.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: RecipesListComponent},
+  {path: 'recipes' , component: RecipesListComponent},
+  {path: 'shoppinglists' , component: ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
